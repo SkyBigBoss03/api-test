2ab2f89a8ebe21de46e006739ab09e295f939e27
SkyBigBoss03 <Mr_lin_zhang@163.com>
2018-08-27 21:22:37 +0800

    test

---
+++ src/main/java/com/ifly/manage/pojo/Item.java
@@ -0,0 +1,16 @@
+package com.ifly.manage.pojo;
+
+public class Item {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
